b9d4f2e6ae99b4ad4b1abaa58e06eae9ce6af069
Bastien <45488913+BastienBiblocque@users.noreply.github.com>
2022-07-20 11:28:47 +0200

    🎨 patch de la navbar

--- www.covroom.com/src/Components/Header/index.js
+++ www.covroom.com/src/Components/Header/index.js
@@ -1,48 +1,79 @@
 import React, {useState} from 'react';
 import logo from '../../img/logo.png'
 import {Link} from "react-router-dom";
-import {MdViewHeadline} from "react-icons/md";
 
 function Header() {
-    const [usersLog, setUserLog] = useState(false);
+    const [usersLog] = useState(false);
     return (
-        <header>
-            <div className="bg-white flex">
-                <div className="max-w-2xl mx-auto">
-                    <div className="flex">
-                        <img className="h-16 pr-32" src={logo} alt="logo"/>
-                        <div className="flex space-x-8 my-auto">
-                            <Link to="/search" className="text-primary">
-                                Rechercher
-                            </Link>
-                            <Link to="/create" className="text-primary">
-                                Publier un trajet
-                            </Link>
-                        </div>
-                        <div className="dropdown dropdown-end my-auto">
-                            <div tabIndex="0" className="cursor-pointer bg-[#FFFFFF] rounded-xl p-3"><MdViewHeadline/>
-                            </div>
-                            <ul tabIndex="0" className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52">
-                                {usersLog ? <>
-                                        <li><Link to="/profil">Mon profil</Link></li>
-                                        <li><Link to="/profil">Mes alertes</Link></li>
-                                        <li><Link to="/search">Mes trajets</Link></li>
-                                        <li><Link to="/search">Mes reservations</Link></li>
-                                        <li><Link to="/search">Mes voitures</Link></li>
-                                        <li><Link to="/search">Mes messages</Link></li>
-                                        <li><Link to="/search">Mes favoris</Link></li>
-                                        <li><Link to="/search">Déconnexion</Link></li>
-                                    </> :
-                                    <>
-                                        <li><Link to="/login">Connexion</Link></li>
-                                        <li><Link to="/signin">Inscription</Link></li>
-                                    </>}
-                            </ul>
-                        </div>
-                    </div>
+        <div className="navbar bg-base-100">
+            <div className="navbar-start">
+                <div className="dropdown">
+                    <label tabIndex="0" className="btn btn-ghost md:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24"
+                             stroke="currentColor">
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
+                                  d="M4 6h16M4 12h8m-8 6h16"/>
+                        </svg>
+                    </label>
+                    <ul tabIndex="0"
+                        className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                        <li><Link to="/search">
+                            Rechercher
+                        </Link></li>
+                        <li><Link to="/create">
+                            Publier un trajet
+                        </Link></li>
+                    </ul>
+                </div>
+                <Link to="/" className="text-primary hidden md:flex">
+                    <img className="h-16" src={logo} alt="logo"/>
+                </Link>
+            </div>
+            <div className="navbar-center">
+                <div className="flex md:hidden">
+                    <Link to="/">
+                        <img className="h-16" src={logo} alt="logo"/>
+                    </Link>
+                </div>
+                <div className="hidden md:flex">
+                    <ul className="menu menu-horizontal p-0">
+                        <li><Link to="/search" className="text-primary">
+                            Rechercher
+                        </Link></li>
+                        <li><Link to="/create" className="text-primary">
+                            Publier un trajet
+                        </Link></li>
+                    </ul>
                 </div>
             </div>
-        </header>
+            <div className="navbar-end">
+                <div className="dropdown dropdown-end my-auto">
+                    <label tabIndex="0" className="btn btn-ghost">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24"
+                             stroke="currentColor">
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
+                                  d="M4 6h16M4 12h8m-8 6h16"/>
+                        </svg>
+                    </label>
+                    <ul tabIndex="0" className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52">
+                        {usersLog ? <>
+                                <li><Link to="/profil">Mon profil</Link></li>
+                                <li><Link to="/profil">Mes alertes</Link></li>
+                                <li><Link to="/search">Mes trajets</Link></li>
+                                <li><Link to="/search">Mes reservations</Link></li>
+                                <li><Link to="/search">Mes voitures</Link></li>
+                                <li><Link to="/search">Mes messages</Link></li>
+                                <li><Link to="/search">Mes favoris</Link></li>
+                                <li><Link to="/search">Déconnexion</Link></li>
+                            </> :
+                            <>
+                                <li><Link to="/login">Connexion</Link></li>
+                                <li><Link to="/signin">Inscription</Link></li>
+                            </>}
+                    </ul>
+                </div>
+            </div>
+        </div>
     );
 }
 
