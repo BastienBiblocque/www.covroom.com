c64508de6ad2c9eb64284c70244156781b6086f9
Bastien <45488913+BastienBiblocque@users.noreply.github.com>
2022-09-16 10:40:22 +0200

    🎨 patch user card en responsive

--- www.covroom.com/src/Pages/signin.js
+++ www.covroom.com/src/Pages/signin.js
@@ -4,6 +4,7 @@ import Header from "../Components/Header";
 import SignInForm from "../Components/SignInForm";
 import Footer from "../Components/Footer";
 import MoreInformation from "../Components/SignInForm/moreInformation";
+import axios from "axios";
 
 
 
@@ -14,8 +15,13 @@ function SignIn() {
         if (type === 'sign') {
             return (<SignInForm setType={setType}/>)
         } else if (type === 'moreInformation') {
-            return(<MoreInformation setType={setType}/>)
+            return(<MoreInformation setType={setType} postSign={PostSign}/>)
         }
+    }
+
+    const PostSign = async () => {
+        const newUsers = await axios.post('http://127.0.0.1:8000/user/');
+        console.log(newUsers)
     }
 
     return (
